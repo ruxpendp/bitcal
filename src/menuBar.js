73603521ca243fba7da5a9136127a513acb09786
ruxpendp <ruxpendp@gmail.com>
2020-10-10 15:49:59 -0700

    basic error handling

--- src/menuBar.js
+++ src/menuBar.js
@@ -11,20 +11,30 @@ const { calendars = [] } = require('../config');
 const renderIcon = icon => [{ text: '', templateImage: icon }, bitbar.separator];
 
 const renderMenuBar = async () => {
-  const { buckets, multiBucketEvents, timeMin, timeMax } = getActiveView();
+  try {
+    const { buckets, multiBucketEvents, timeMin, timeMax } = getActiveView();
 
-  const events = await getEvents({
-    calendarIds: calendars.reduce((ids, { active, id }) => active ? [...ids, id] : ids, []),
-    timeMin,
-    timeMax
-  });
+    const events = await getEvents({
+      calendarIds: calendars.reduce((ids, { active, id }) => active ? [...ids, id] : ids, []),
+      timeMin,
+      timeMax
+    });
 
-  return [
-    ...renderIcon(iconActive),
-    ...renderEventBuckets({ buckets, events, multiBucketEvents }),
-    renderViewsMenu(),
-    renderCalendarConfigMenu()
-  ];
+    return [
+      ...renderIcon(iconActive),
+      ...renderEventBuckets({ buckets, events, multiBucketEvents }),
+      renderViewsMenu(),
+      renderCalendarConfigMenu()
+    ];
+  } catch (error) {
+    return [
+      ...renderIcon(iconInactive),
+      { text: 'Could Not Fetch Agenda' },
+      bitbar.separator,
+      { text: 'Debug', submenu: [{ text: error.stack }] }
+    ];
+  }
+
 };
 
 exports.renderMenuBar = renderMenuBar;
