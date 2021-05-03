6125524ebc2d9ac777f38a678e3e7bc32c024f6a
ruxpendp <ruxpendp@gmail.com>
2021-05-03 14:15:38 -0700

    new login method.

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,21 @@
 import bitbar from 'bitbar';
 
 import { selectView, toggleCalendar, refreshCalendars } from './configHelpers';
+import { login } from './auth';
 import { renderMenuBar } from './menuBar';
 
 interface OptParse {
   'select-view': typeof selectView;
   'toggle-calendar': typeof toggleCalendar;
   'refresh-calendars': typeof refreshCalendars;
+  'login': typeof login;
 }
 
 const optparse: OptParse = {
   'select-view': selectView,
   'toggle-calendar': toggleCalendar,
-  'refresh-calendars': refreshCalendars
+  'refresh-calendars': refreshCalendars,
+  'login': login
 };
 
 const isValidOpt = (opt: string): opt is keyof OptParse => opt in optparse;
